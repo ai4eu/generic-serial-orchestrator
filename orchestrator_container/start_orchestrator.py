3fbf6da218ce5b2133f0a1269dc87751b9d7e541
Tejas Morbagal Harish <tejasmorbagal@gmail.com>
2021-04-08 13:54:29 +0200

    create work_dir
    create work_dir in orchestrator container if not exist

--- orchestrator_container/start_orchestrator.py
+++ orchestrator_container/start_orchestrator.py
@@ -27,6 +27,13 @@ import glob
 curr_dir = os.getcwd()
 work_dir_path = os.path.join(curr_dir,"work_dir")
 
+try:
+    if not os.path.exists('work_dir'):
+        os.makedirs(work_dir_path)
+except OSError as e:
+    print ("Creation of the directory %s failed" % work_dir_path)
+
+
 def string_to_blueprint_json(text):
     """Converts text to json file"""
     path = os.path.join(work_dir_path,"blueprint_gen.json")
